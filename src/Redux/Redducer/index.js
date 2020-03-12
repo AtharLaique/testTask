7425a6b61b783a05fe63e2f6ac8dd402dd0aa6b0
Athar_Laique <athar@softcircles.org>
2020-03-12 18:44:44 +0500

    Error resolved

--- src/Redux/Redducer/index.js
+++ src/Redux/Redducer/index.js
@@ -1,6 +1,6 @@
-// import {combineReducers} from 'redux';
-// import auth from './auth';
+import {combineReducers} from 'redux';
 
-// export default combineReducers({
-//    user:auth
-// });
+
+export default combineReducers({
+  
+});
