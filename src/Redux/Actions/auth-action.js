dcd4f5c975b8862e402ad50a213ae861e26815b0
Athar_Laique <athar@softcircles.org>
2020-03-12 19:37:37 +0500

    AuthAction

---
+++ src/Redux/Actions/auth-action.js
@@ -0,0 +1,7 @@
+ export const AuthAction =(Data , history)=>dispatch=>{
+     //Perform Action and update state by calling dispatch
+//      dispatch({
+//         type:LOGIN_DISPATCH ,
+//         payload:response.data
+//     })
+ }
